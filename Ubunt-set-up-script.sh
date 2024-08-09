8487135dc1e30d48201f2f470e10ca222ae847ba
Ubuntu <ubuntu@ip-172-31-34-146.ap-south-1.compute.internal>
2024-08-09 07:19:53 +0000

    addind userdata script for Ubuntu EC2

---
+++ Ubunt-set-up-script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# this script installs docker and ansible on Ubuntu EC2 instance [ we can add this script as userdata in EC2 instance ]
+# Add Docker's official GPG key:
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo apt-get install ansible -y
+sudo usermod -aG docker $USER  # adding current user to docker
+groups # if you don't find docker in the output try to add user to docker manually and refresh ec2 instance. 
+
